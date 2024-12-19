d7afe0214512bf822795ed1c539450f3ea130cdf
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-12-19 01:47:29 +0100

    [Code] Implemented simple boot loader

--- Assets/_IT/Scripts/Runtime/Boot/BootLoader.cs
+++ Assets/_IT/Scripts/Runtime/Boot/BootLoader.cs
@@ -1,8 +1,33 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using IT.Utils;
 
 namespace IT.Boot
 {
     public class BootLoader : MonoBehaviour
     {
+        // TODO: Load scenes from Addressables or SceneReference
+        [SerializeField] private int[] m_persistentScenesID;
+        [SerializeField] private int m_mainSceneID;
+
+        private async void Start() => await Boot();
+
+        private async Awaitable Boot()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            foreach (int persistentSceneID in m_persistentScenesID)
+            {
+                await SceneManager.LoadSceneAsync(persistentSceneID, LoadSceneMode.Additive).ToTask();
+            }
+
+            Destroy(Camera.main?.gameObject);
+
+            Scene newCurrentScene = await SceneManager.LoadSceneAsync(m_mainSceneID, LoadSceneMode.Additive).ToTask(m_mainSceneID);
+            SceneManager.SetActiveScene(newCurrentScene);
+
+            await SceneManager.UnloadSceneAsync(currentScene).ToTask();
+        }
     }
 }
